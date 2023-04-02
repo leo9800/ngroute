09a0d5c2bf49efc9f275fd27cb0a8a8b0b367363
Leo <i@hardrain980.com>
2023-04-03 01:58:31 +1000

    NgRoute\Parser\StdParser: implemented + tested

---
+++ src/Parser/StdParser.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Leo\NgRoute\Parser;
+
+use Leo\NgRoute\Exceptions\Parser\BracketsMismatchException;
+use Leo\NgRoute\Exceptions\Parser\EmptyRouteException;
+use Leo\NgRoute\Exceptions\Parser\OptionalSegmentInMiddleException;
+use Leo\NgRoute\ParserInterface;
+use Leo\NgRoute\SegmentInterface;
+use Leo\NgRoute\Segments\FixedSegment;
+use Leo\NgRoute\Segments\VariableSegment;
+
+class StdParser implements ParserInterface
+{
+	private const VARIABLE_REGEX = '\{\s*([a-zA-Z_][a-zA-Z0-9_-]*)\s*(?::\s*([^{}]*(?:\{(?-1)\}[^{}]*)*))?\}';
+	public function parse(string $raw_route): array
+	{
+		return array_map(
+			callback:[$this, 'parseSingleRoute'],
+			array:$this->expandOptionalSegments($raw_route)
+		);
+	}
+
+	/**
+	 * Expand route with optional segments into multiple 
+	 * routes without optional segments.
+	 * e.g.
+	 * '/user/{id:\d+}' -> ['/user/{id:\d+}']
+	 * '/tag/{name}[/page/{page:\d+}]' -> ['/tag/{name}', '/tag/{name}/page/{page:\d+}']
+	 * '/cas[ca[ded]]' -> ['/cas', '/casca', '/cascaded']
+	 * @param  string        $raw_route Raw route string with optional segments
+	 * @return array<string>            Raw route strings without optional segments
+	 */
+	private function expandOptionalSegments(string $raw_route): array
+	{
+		$route_without_closing_optional = rtrim($raw_route, ']');
+		$num_optionals = strlen($raw_route) - strlen($route_without_closing_optional);
+
+		// Split on '[' wile skipping other slices
+		// i.e. variable segments, fixed segments
+		$slices = preg_split(
+			pattern:'~'.self::VARIABLE_REGEX.'(*SKIP)(*F) | \[~x',
+			subject:$route_without_closing_optional,
+		);
+
+		// TBD: handle preg_split() returning false ...
+		assert(is_array($slices));
+
+		if ($num_optionals != count($slices) - 1) {
+			// If there are any ']' in the middle of the route ...
+			if (preg_match(
+				pattern:'~'.self::VARIABLE_REGEX.'(*SKIP)(*F)  | \]~x',
+				subject:$route_without_closing_optional,
+			))
+				throw new OptionalSegmentInMiddleException();
+
+			// Otherwise, there's at lease a '[]' brackets not closed ...
+			throw new BracketsMismatchException();
+		}
+
+		$glued = [];
+
+		foreach ($slices as $i => $s1) {
+			if ($s1 === '' && $i !== 0)
+				throw new EmptyRouteException();
+
+			$o = '';
+
+			foreach ($slices as $j => $s2) {
+				$o .= $s2;
+				if ($i == $j)
+					break;
+			}
+
+			$glued[] = $o;
+		}
+
+		return $glued;
+	}
+
+	/**
+	 * Parse single route without optional segment into 
+	 * array of route segments
+	 * @param  string                  $route Route string without optional segment
+	 * @return array<SegmentInterface>        Route segments
+	 */
+	private function parseSingleRoute(string $route): array
+	{
+		// Find all variable segments in route string
+		$r = preg_match_all(
+			pattern:'~'.self::VARIABLE_REGEX.'~',
+			subject:$route,
+			matches:$matches,
+			flags:PREG_OFFSET_CAPTURE | PREG_SET_ORDER,
+		);
+
+		// TBD: $r could be false, if recursion limit has been reached ...
+		assert(is_int($r));
+
+		// No variable segment matched, only 1 fixed segment in this route.
+		if ($r === 0)
+			return [new FixedSegment($route)];
+
+		$offset = 0;
+		$segments = [];
+
+		foreach ($matches as $set) {
+			// A fixed segment is found before the current variable segment
+			if ($set[0][1] > $offset)
+				$segments[] = new FixedSegment(substr(
+					string:$route,
+					offset:$offset,
+					length:$set[0][1] - $offset,
+				));
+
+			// Parse the current variable segment
+			$segments[] = new VariableSegment(
+				name:$set[1][0],
+				match:trim($set[2][0] ?? ''), // use default if not specified
+			);
+
+			// Move offset to the end of current variable segment
+			$offset = $set[0][1] + strlen($set[0][0]);
+		}
+
+		// Parse fixed segment at the end of route string if it exist
+		if ($offset !== strlen($route))
+			$segments[] = new FixedSegment(substr(
+				string:$route,
+				offset:$offset,
+			));
+
+		return $segments;
+	}
+}
---
+++ tests/Parser/StdParserTest.php
@@ -0,0 +1,232 @@
+<?php
+
+use Leo\NgRoute\Exceptions\Parser\BracketsMismatchException;
+use Leo\NgRoute\Exceptions\Parser\EmptyRouteException;
+use Leo\NgRoute\Exceptions\Parser\OptionalSegmentInMiddleException;
+use Leo\NgRoute\Parser\StdParser;
+use Leo\NgRoute\Segments\FixedSegment;
+use Leo\NgRoute\Segments\VariableSegment;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @testdox Leo\NgRoute\Parser\StdParser
+ */
+class StdParserTest extends TestCase
+{
+	/**
+	 * @testdox Parse valid route string: $raw_route
+	 * @dataProvider provideValidRouteData
+	 * @param array<mixed> $route
+	 */
+	public function testParseValidRoute(string $raw_route, array $route): void
+	{
+		$sp = new StdParser();
+		$this->assertEquals($route, $sp->parse($raw_route));
+	}
+
+	/**
+	 * @testdox Throw exception on invalid route string: $raw_route
+	 * @dataProvider provideInvalidRouteData
+	 * @param class-string<Throwable> $exception
+	 */
+	public function testThrowExceptionOnInvalidRoute(string $raw_route, string $exception): void
+	{
+		$this->expectException($exception);
+		$sp = new StdParser();
+		$sp->parse($raw_route);
+	}
+
+	/**
+	 * @return array<mixed>
+	 */
+	public static function provideValidRouteData(): array
+	{
+		return [
+			[
+				'/test',
+				[
+					[
+						new FixedSegment('/test'),
+					]
+				]
+			],
+			[
+				'/test/{param}',
+				[
+					[
+						new FixedSegment('/test/'),
+						new VariableSegment('param', VariableSegment::DEFAULT_MATCH),
+					],
+				],
+			],
+			[
+				'/te{ param }st',
+				[
+					[
+						new FixedSegment('/te'),
+						new VariableSegment('param', VariableSegment::DEFAULT_MATCH),
+						new FixedSegment('st'),
+					],
+				],
+			],
+			[
+				'/test/{param1}/test2/{param2}',
+				[
+					[
+						new FixedSegment('/test/'),
+						new VariableSegment('param1', VariableSegment::DEFAULT_MATCH),
+						new FixedSegment('/test2/'),
+						new VariableSegment('param2', VariableSegment::DEFAULT_MATCH),
+					],
+				],
+			],
+			[
+				'/test/{param:\d+}',
+				[
+					[
+						new FixedSegment('/test/'),
+						new VariableSegment('param', '\d+'),
+					],
+				],
+			],
+			[
+				'/test/{ param : \d{1,9} }',
+				[
+					[
+						new FixedSegment('/test/'),
+						new VariableSegment('param', '\d{1,9}'),
+					],
+				],
+			],
+			[
+				'/test[opt]',
+				[
+					[
+						new FixedSegment('/test'),
+					],
+					[
+						new FixedSegment('/testopt'),
+					],
+				],
+			],
+			[
+				'/test[/{param}]',
+				[
+					[
+						new FixedSegment('/test'),
+					],
+					[
+						new FixedSegment('/test/'),
+						new VariableSegment('param', VariableSegment::DEFAULT_MATCH),
+					],
+				],
+			],
+			[
+				'/{param}[opt]',
+				[
+					[
+						new FixedSegment('/'),
+						new VariableSegment('param', VariableSegment::DEFAULT_MATCH),
+					],
+					[
+						new FixedSegment('/'),
+						new VariableSegment('param', VariableSegment::DEFAULT_MATCH),
+						new FixedSegment('opt'),
+					],
+				],
+			],
+			[
+				'/test[/{name}[/{id:[0-9]+}]]',
+				[
+					[
+						new FixedSegment('/test'),
+					],
+					[
+						new FixedSegment('/test/'),
+						new VariableSegment('name', VariableSegment::DEFAULT_MATCH),
+					],
+					[
+						new FixedSegment('/test/'),
+						new VariableSegment('name', VariableSegment::DEFAULT_MATCH),
+						new FixedSegment('/'),
+						new VariableSegment('id', '[0-9]+')
+					],
+				],
+			],
+			[
+				'',
+				[
+					[
+						new FixedSegment(''),
+					],
+				],
+			],
+			[
+				'[test]',
+				[
+					[
+						new FixedSegment(''),
+					],
+					[
+						new FixedSegment('test'),
+					],
+				],
+			],
+			[
+				'/{foo-bar}',
+				[
+					[
+						new FixedSegment('/'),
+						new VariableSegment('foo-bar', VariableSegment::DEFAULT_MATCH),
+					],
+				],
+			],
+			[
+				'/{_foo:.*}',
+				[
+					[
+						new FixedSegment('/'),
+						new VariableSegment('_foo', '.*'),
+					],
+				],
+			],
+		];
+	}
+
+	/**
+	 * @return array<mixed>
+	 */
+	public static function provideInvalidRouteData(): array
+	{
+		return [
+			[
+				'/test[opt',
+				BracketsMismatchException::class,
+			],
+			[
+				'/test[opt[opt2]',
+				BracketsMismatchException::class,
+			],
+			[
+				'/testopt]',
+				BracketsMismatchException::class,
+			],
+			[
+				'/test[]',
+				EmptyRouteException::class,
+			],
+			[
+				'/test[[opt]]',
+				EmptyRouteException::class,
+			],
+			[
+				'[[test]]',
+				EmptyRouteException::class,
+			],
+			[
+				'/test[/opt]/required',
+				OptionalSegmentInMiddleException::class,
+			],
+		];
+	}
+}
