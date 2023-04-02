de277dae77d3c9db441fa7b8f36f4bf14fdacbb5
Leo <i@hardrain980.com>
2023-04-03 00:48:22 +1000

    NgRoute\Exceptions\Parser: implement parser exceptions

---
+++ src/Exceptions/Parser/EmptyRouteException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Leo\NgRoute\Exceptions\Parser;
+
+use Leo\NgRoute\Exceptions\ParserException;
+
+class EmptyRouteException extends ParserException
+{
+
+}
---
+++ src/Exceptions/Parser/OptionSegmentInMiddleException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Leo\NgRoute\Exceptions\Parser;
+
+use Leo\NgRoute\Exceptions\ParserException;
+
+class OptionalSegmentInMiddleException extends ParserException
+{
+
+}
